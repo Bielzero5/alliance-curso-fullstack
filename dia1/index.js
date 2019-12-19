dc6d4371d2f8b18cc51ba25207b78f5f701dc5d9
Diego Yegros <dyegros@gmail.com>
2019-12-19 17:35:25 -0300

    Tudo antes da avaliação

--- dia1/index.js
+++ dia1/index.js
@@ -1,11 +1,14 @@
 import express from 'express';
 import calculadora from './calculadora';
+import cors from 'cors';
 
 const app = express();
 app.use(express.json());
+app.use(cors());
+
 app.use('/calculadora', calculadora);
 
-app.get('/axios.js', (req, res) => res.sendFile(__dirname + '/axios.js'));
+app.get('/axios.js', (req, res) => res.sendFile(__dirname + '/axios.js'));app.get('/axios.js', (req, res) => res.sendFile(__dirname + '/axios.js'));
 
 app.listen(3000, () =>
     console.log('Servidor rodando na porta 30000!'),
